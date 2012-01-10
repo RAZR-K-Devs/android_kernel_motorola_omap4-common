4a7dde237c36136b4f3a40e0f2a430ec715c6331
Vincent Guittot <vincent.guittot@linaro.org>
2012-01-10 18:35:51 +0100

    ARM: cpu topology: Add asym topology flag for using cpu0 1st
    Modify the CPU sched_domain flags in powersave mode for using the cpu0 in ched_mc powersave mode
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: nicholas <firepoison35@gmail.com>
    
    Conflicts:
    	arch/arm/include/asm/topology.h
    
    Conflicts:
    	arch/arm/include/asm/topology.h
    
    Change-Id: Ibac5293abadd98130b888032eff26d1dffeb3a9c

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -243,6 +243,17 @@ int arch_sd_sibling_asym_packing(void)
 }
 
 /*
+ * sched_domain flag configuration
+ */
+/* TODO add a config flag for this function */
+int arch_sd_sibling_asym_packing(void)
+{
+	if (sched_smt_power_savings || sched_mc_power_savings)
+		return SD_ASYM_PACKING;
+	return 0;
+}
+
+/*
  * default topology function
  */
 
