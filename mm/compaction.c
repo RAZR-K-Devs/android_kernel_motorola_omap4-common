669a4b086dd2cd2598f865120019991a95e20ff5
dtrail <mnl.manz@gmail.com>
2014-04-15 22:20:35 +0200

    mm: compaction: fix echo 1 > compact_memory return error issue
    This tells system return 3(COMPACT_COMPLETE) after write data to
    compact_memory.
    
    The fix is to make the system just return 0 instead 3(COMPACT_COMPLETE)
    from sysctl_compaction_handler after compaction_nodes finished.

--- mm/compaction.c
+++ mm/compaction.c
@@ -722,7 +722,6 @@ static int compact_nodes(void)
 	for_each_online_node(nid)
 		compact_node(nid);
 
-	return COMPACT_COMPLETE;
 }
 
 /* The written value is actually unused, all memory is compacted */
@@ -733,7 +732,7 @@ int sysctl_compaction_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *length, loff_t *ppos)
 {
 	if (write)
-		return compact_nodes();
+		compact_nodes(true);
 
 	return 0;
 }
