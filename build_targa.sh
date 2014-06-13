922eb33b85dcc73aa295a802cb08838318f7aa6e
dtrail <mnl.manz@gmail.com>
2014-06-13 16:41:31 +0200

    Update JBX 3.3.5

--- build_targa.sh
+++ build_targa.sh
@@ -46,7 +46,7 @@ export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
 
 # export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-unknown-linux-gnueabi-standard_4.7.2
-export LOCALVERSION="-JBX-3.2-Hybrid-Bionic-4.4"
+export LOCALVERSION="-JBX-3.3-Hybrid-Bionic-4.4"
 # Choose build option
 cd /data/4.4
 while true; do
@@ -91,8 +91,8 @@ echo "Packaging flashable Zip file..."
 echo " "
 
 cd /home/dtrail/android/built/4.4/3.0/targa/rls
-zip -r "JBX-Kernel-3.2-Hybrid-Targa-4.4_$(date +"%Y-%m-%d").zip" *
-mv "JBX-Kernel-3.2-Hybrid-Targa-4.4_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
+zip -r "JBX-Kernel-3.3-Hybrid-Targa-4.4_$(date +"%Y-%m-%d").zip" *
+mv "JBX-Kernel-3.3-Hybrid-Targa-4.4_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
 
 # Exporting changelog to file
 cd /home/dtrail/android/android_kernel_motorola_omap4-common
