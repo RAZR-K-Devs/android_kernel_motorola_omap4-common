1a03ad69c5c3cdc7293b52460fd3d06c2f404e10
dtrail <mnl.manz@gmail.com>
2014-06-22 02:14:27 +0200

    JBX-Kernel 3.4

--- build_edison.sh
+++ build_edison.sh
@@ -46,7 +46,7 @@ export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
 
 # export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-unknown-linux-gnueabi-standard_4.7.2
-export LOCALVERSION="-JBX-3.3-Hybrid-Atrix2-4.4"
+export LOCALVERSION="-JBX-3.4-Hybrid-Atrix2-4.4"
 export BOARD_HAS_SDCARD_INTERNAL=false
 # Choose build option
 cd /data/4.4
@@ -90,8 +90,8 @@ echo "Packaging flashable Zip file..."
 echo " "
 
 cd /home/dtrail/android/built/4.4/3.0/edison/rls
-zip -r "JBX-Kernel-3.3-Hybrid-Edison-4.4_$(date +"%Y-%m-%d").zip" *
-mv "JBX-Kernel-3.3-Hybrid-Edison-4.4_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
+zip -r "JBX-Kernel-3.4-Hybrid-Edison-4.4_$(date +"%Y-%m-%d").zip" *
+mv "JBX-Kernel-3.4-Hybrid-Edison-4.4_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
 
 # Exporting changelog to file
 cd /home/dtrail/android/android_kernel_motorola_omap4-common
