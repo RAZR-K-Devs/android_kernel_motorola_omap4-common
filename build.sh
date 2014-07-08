0ace886e30647b42d71d6cc97cb92cdea3f62775
dtrail <mnl.manz@gmail.com>
2014-07-08 22:40:15 +0200

    defconfig: make best config universal

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
 
 # export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-unknown-linux-gnueabi-standard_4.7.2
-export LOCALVERSION="-JBX-3.4-Hybrid-Razr-4.4"
+export LOCALVERSION="-JBX-3.5-Hybrid-Razr-4.4"
 # Choose build option
 cd /media/dtrail/_home/4.4
 while true; do
@@ -90,8 +90,8 @@ echo "Packaging flashable Zip file..."
 echo " "
 
 cd /home/dtrail/android/built/4.4/3.0/rls
-zip -r "JBX-Kernel-3.4-Hybrid-Spyder-4.4_$(date +"%Y-%m-%d").zip" *
-mv "JBX-Kernel-3.4-Hybrid-Spyder-4.4_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
+zip -r "JBX-Kernel-3.5-Hybrid-Spyder-4.4_$(date +"%Y-%m-%d").zip" *
+mv "JBX-Kernel-3.5-Hybrid-Spyder-4.4_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
 
 # Exporting changelog to file
 cd /home/dtrail/android/android_kernel_motorola_omap4-common
